98ef18e717e6b89a464b4113e6a00d37ef791a5b
Nurdoolot <coolishcosma@gmail.com>
2026-09-24 09:28:12 +0600

    lol

---
+++ src/B/Main.java
@@ -0,0 +1,15 @@
+package B;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        System.out.println("The next number for the number " + n + " is " + (n + 1) + ".");
+
+        System.out.println("The previous number for the number " + n + " is " + (n - 1) + ".");
+    }
+}
