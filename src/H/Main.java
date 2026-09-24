ed15fb553bf2e7e62cb76f5e7b4b18e3ec902cd1
Nurdoolot <coolishcosma@gmail.com>
2026-09-24 10:07:27 +0600

    lol

--- src/H/Main.java
+++ src/H/Main.java
@@ -7,8 +7,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-
-
         int tens = (n / 10) % 10;
 
         System.out.println(tens);
