0cc005a591cfedf5a40e9049591263d4a55a387c
Nurdoolot <coolishcosma@gmail.com>
2026-09-24 09:56:54 +0600

    lol

--- src/C/Main.java
+++ src/C/Main.java
@@ -1,2 +1,17 @@
 package C;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int k = scanner.nextInt();
+
+        int result = k / n;
+
+        System.out.println(result);
+    }
+}
